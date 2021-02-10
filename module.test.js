910bc53f6ac09e20acf0b6c0ad041c468edbdd67
Erdem Temizdemir <55912553+gh03ty@users.noreply.github.com>
2021-02-10 14:53:43 +0100

    Test implemented

---
+++ module.test.js
@@ -0,0 +1,36 @@
+
+const modulo = require('./modulo');
+
+describe('check operator', () => {
+    it('operator should be %', () => {
+        expect(modulo.operator).toBe('%');
+    });
+});
+
+describe('check function', () => {
+
+    it('9 % 8 should be 1', () => {
+        // Arrange
+        const x = 9;
+        const y = 8;
+
+        // Act
+        const result = modulo.fn(x, y);
+
+        // Assert
+        expect(result).toBe(1);
+    });
+
+    it('-9 % 8 should be -1', () => {
+        // Arrange
+        const x = -9;
+        const y = 8;
+
+        // Act
+        const result = modulo.fn(x, y);
+
+        // Assert
+        expect(result).toBe(-1);
+    });
+
+});
